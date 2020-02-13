78b28e3bdc2d606a8b8d1668c9a7e27b881ca63a
Tommy Long Pham <tpham@smugmug.com>
2020-02-13 10:52:56 -0800

    add graphiql

--- server/app.js
+++ server/app.js
@@ -4,7 +4,10 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({ schema }));
+app.use('/graphql', graphqlHTTP({ 
+  schema,
+  graphiql: true,
+}));
 
 app.listen(4000, () => {
   console.log('cowman');
