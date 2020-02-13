5b1c342c29b79cf8797e43fcc41c339abc83bf0b
Tommy Long Pham <tpham@smugmug.com>
2020-02-13 10:55:35 -0800

    author schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLString,
   GraphQLID,
   GraphQLSchema,
+  GraphQLInt,
 } = graphql;
 
 const dummyBooks = [
@@ -13,12 +14,27 @@ const dummyBooks = [
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ];
 
+const dummyAuthors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' }
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
